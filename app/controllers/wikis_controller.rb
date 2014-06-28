cd36318a3eda329c55d8f218dfb0d2075216216f
nikhils777 <nsharmatennis@gmail.com>
2014-06-28 14:44:29 -0400

    fixed authorizations

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,7 +1,6 @@
 class WikisController < ApplicationController
   def index
     @wikis = Wiki.all.visible_to
-    authorize @wikis
   end
 
   def new
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -2,4 +2,7 @@ class WikiPolicy < ApplicationPolicy
   def index?
     true
   end
+  def collaborate?
+    (record.user == user || user.role?(:admin)) || (@wiki.collaborate.include?(current_user.email) )
+  end
 end
